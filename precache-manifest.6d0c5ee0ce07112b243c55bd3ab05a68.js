2b967f4d1b1ff75c437cc789dcd7613ed32bfe2f
TatsianaH <ourflsun@gmail.com>
2020-02-22 12:34:42 -0500

    Updates

---
+++ precache-manifest.6d0c5ee0ce07112b243c55bd3ab05a68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "113b9fe8bc06d71632119fddfeceaed2",
+    "url": "/counter-app/index.html"
+  },
+  {
+    "revision": "5c3ab0659fc68272b1b5",
+    "url": "/counter-app/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "aca82bfa409cb19c1076",
+    "url": "/counter-app/static/js/2.6924ed70.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/counter-app/static/js/2.6924ed70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c3ab0659fc68272b1b5",
+    "url": "/counter-app/static/js/main.8cb537a0.chunk.js"
+  },
+  {
+    "revision": "1cb6a0601cc0d8f5ab21",
+    "url": "/counter-app/static/js/runtime-main.06758c85.js"
+  }
+]);
